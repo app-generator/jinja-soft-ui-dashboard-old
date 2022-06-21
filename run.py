5bb192c132c762050cf90f7fa715b17ba83b34cb
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-21 10:03:53 +0300

    Update Jinja Codebase

--- run.py
+++ run.py
@@ -3,10 +3,18 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from flask_minify  import Minify
+
 from apps import app
 
-app.logger.info('DEBUG       = ' + str( app.config['DEBUG'] ))
-app.logger.info('ASSETS_ROOT = ' + app.config['ASSETS_ROOT'])
+DEBUG = app.config['DEBUG'] 
+
+if not DEBUG:
+    Minify(app=app, html=True, js=False, cssless=False)
+
+app.logger.info('DEBUG            = ' + str( DEBUG )                 )
+app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
+app.logger.info('ASSETS_ROOT      = ' + app.config['ASSETS_ROOT']    )
 
 if __name__ == "__main__":
     app.run()
